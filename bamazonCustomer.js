2d6beed0e4f92fc5908ebb767cf2af49d6ce437a
Yuta Tamura <yutaatamura@gmail.com>
2018-04-19 11:26:10 -0500

    Create productID and quantity prompt funtionalities

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,6 +13,10 @@ connection.connect(function(err) {
     if(err) throw err; 
 });
 
+var productID;
+var selProductPrice;
+var selProductStock;
+
 function displayProducts() {
     inquirer.prompt({
         name:"welcome",
@@ -22,7 +26,18 @@ function displayProducts() {
         if (answer.welcome) {
             console.log("Here is today's inventory...");
             var query = "SELECT item_id, product_name, price FROM products";
-            connection.query(query, {})
+            connection.query(query, function(err, res) {
+                if(err) throw err;
+                console.log(`
+Item ID: ${res.item_id}
+Item Name: ${res.product_name}
+Price: ${res.price}
+`)
+                productIDPrompt();
+            })
+        } else {
+            console.log("Sorry, please input a valid item ID number.")
+            displayProducts();
         }
     })
 }
@@ -32,16 +47,34 @@ function productIDPrompt() {
         name: "ask",
         message: "Please input the ID of the product you would like to buy."
     }).then(function(answer) {
-
+        productID = answer.ask;
+        var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?";
+        connection.query(query, { item_id: answer.ask}, function(err, res) {
+            if (err) throw err;
+            selProductPrice = res.price;
+            selProductStock = res.stock_quantity;
+            console.log(`Here is the requested item information: ${res}`);
+            quantityPrompt(selProductStock);
+        })
     })
 }
 
-function quantityPrompt() {
+function quantityPrompt(stock) {
     inquirer.prompt({
         name: "quantity",
         message: "How many units of the product would you like to buy?"
     }).then(function(answer) {
-
+        if (answer.name > stock) {
+        console.log("Thank you! Your order has been placed.");
+        var query = "UPDATE products SET ? WHERE ?";
+        connection.query(query, {stock_quantity: stock_quantity - answer.name, item_id = productID}, function(err, res) {
+            if (err) throw err;
+            var total = selProductPrice * answer.name;
+            console.log(`Your total order is: ${total}`); 
+        })
+        } else {
+            console.log("Insufficient quantity!");
+        }
     })
 }
 
