d3f98cda2666db40f6d159f236c07da957c0c0b0
Yuta <yutaatamura@gmail.com>
2018-04-19 15:36:27 -0500

    Complete basic functionalities

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,19 +29,23 @@ function displayProducts() {
             var query = "SELECT item_id, product_name, price FROM products";
             connection.query(query, function(err, res) {
                 if(err) throw err;
-                console.log(`
-Item ID: ${res.item_id}
-Item Name: ${res.product_name}
-Price: ${res.price}
+                for (var i=0; i<res.length; i++) {
+                console.log(`------------------------------------------------   
+Item ID: ${res[i].item_id}
+Item Name: ${res[i].product_name}
+Price: $${res[i].price}
 `)
+                }
                 productIDPrompt();
             })
         } else {
-            console.log("Sorry, please input a valid item ID number.")
-            displayProducts();
+            console.log("Thank you for shopping at Bamazon! Have a nice day.");
+            return;
         }
     })
 }
+// console.log("Sorry, please input a valid item ID number.")
+//             displayProducts();
 
 function productIDPrompt() {
     inquirer.prompt({
@@ -52,9 +56,20 @@ function productIDPrompt() {
         var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?";
         connection.query(query, { item_id: answer.ask}, function(err, res) {
             if (err) throw err;
-            selProductPrice = res.price;
-            selProductStock = res.stock_quantity;
-            console.log(`Here is the requested item information: ${res}`);
+            selProductPrice = res[0].price;
+            selProductStock = res[0].stock_quantity;
+            
+            console.log(`Here is the requested item information: 
+________________________________________
+
+Item ID: ${res[0].item_id}
+Item Name: ${(res[0].product_name)}
+Price: $${res[0].price}
+Stock: ${res[0].stock_quantity}
+________________________________________
+`);
+            console.log(selProductPrice);
+            console.log(selProductStock);
             quantityPrompt(selProductStock);
         })
     })
@@ -65,17 +80,34 @@ function quantityPrompt(stock) {
         name: "quantity",
         message: "How many units of the product would you like to buy?"
     }).then(function(answer) {
-        if (answer.name > stock) {
+        
+        if (answer.quantity < stock) {
         console.log("Thank you! Your order has been placed.");
         var query = "UPDATE products SET ? WHERE ?";
-        connection.query(query, {stock_quantity: stock_quantity - answer.name, item_id: productID}, function(err, res) {
+        connection.query(query, [{stock_quantity: stock - answer.quantity}, {item_id: productID}], function(err, res) {
             if (err) throw err;
-            var total = selProductPrice * answer.name;
-            console.log(`Your total order is: ${total}`); 
+            var total = selProductPrice * answer.quantity;
+            console.log(`Your total order is: $${total}`); 
+            reOrder();
         })
         } else {
             console.log("Insufficient quantity!");
         }
     })
 }
+
+function reOrder() {
+    inquirer.prompt({
+        name: "reorder",
+        type: "confirm",
+        message: "Would you like to order another item?"
+    }).then(function(answer) {
+        if (answer.reorder) {
+            displayProducts();
+        } else {
+            console.log("Thank you for shopping at Bamazon! Have a nice day.");
+            return;
+        }
+    })
+};
 
